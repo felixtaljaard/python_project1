1016387fc781e01eac920a88c13aa5ecfaeaa46e
EC2 Default User <ec2-user@ip-172-31-17-10.ec2.internal>
2022-06-07 12:39:49 +0000

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+# we want to print 'hello to ______'
+
+# youtuber = "feely"
+
+# print("hello to " + youtuber)
+# print ("hello to {}".format(youtuber))
+# print(f"hello to {youtuber}")
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous peron: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb}.Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
